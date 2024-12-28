6b8617934098ee07f76d81b5fd39fd350340afa4
Kpovey115 <povey.kyle@gmail.com>
2024-12-28 18:43:49 +0000

    added circleshape class

---
+++ circleshape.py
@@ -0,0 +1,29 @@
+import pygame
+
+# circle shape will be the base class for the game, asteroids and the ship will be a circle
+# it will also inherit from pygames sprites class
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+
+        #figoure out later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+
+        self.position = pygame.Vector2(x,y)
+        self.velocity = py.Vector2(0,0)
+        self.radius = radius
+
+
+
+    def draw(self, screen):
+        # child classes will override
+        pass
+
+
+    def update(self, dt):
+        #child classes will override
+        pass
